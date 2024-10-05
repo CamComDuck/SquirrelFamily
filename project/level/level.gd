b8b080566a9379167da220de4963b22ca10ff910
Camden Hovell <camdenhovell@gmail.com>
2024-10-05 15:20:32 -0400

    Add fish win condition

--- project/level/level.gd
+++ project/level/level.gd
@@ -1,11 +1,35 @@
 extends Node2D
 
+var _max_x := 2250
+var _min_x := -975
+
 @onready var game_lost_label: Label = $GameLostLabel
 @onready var camera_2d: Camera2D = $Camera2D
 @onready var squirrel: Squirrel = $Squirrel
+@onready var fish: Sprite2D = $Fish
+@onready var pond_dirty: StaticBody2D = $PondDirty
+@onready var pond_clean: StaticBody2D = $PondClean
+
+
+
+func _ready() -> void:
+	var _pond_dirty_x = randf_range(_min_x + 1001, _max_x - 1001)
+	pond_dirty.set_position(Vector2(_pond_dirty_x, 745))
+	fish.position = pond_dirty.position
+	
+	var random_1 = randf_range(_min_x, _pond_dirty_x - 1000)
+	var random_2 = randf_range(_pond_dirty_x + 1000, _max_x)
+	var random_3 = randi_range(0, 1)
+	
+	if random_3 == 0:
+		pond_clean.set_position(Vector2(random_1, 745))
+	else:
+		pond_clean.set_position(Vector2(random_2, 745))
+
 
 func _physics_process(_delta: float) -> void:
 	camera_2d.position.x = clampf(squirrel.position.x, -465, 1750)
+
 
 func _on_owl_game_lost() -> void:
 	game_lost_label.show()
--- project/squirrel/squirrel.gd
+++ project/squirrel/squirrel.gd
@@ -10,11 +10,21 @@ var _can_move := true
 var _hiding_spot_position : Vector2
 var _is_hiding := false
 
+var _fish_in_hand := false
+var _fish_in_dirty_pond := true
+
+@onready var shape_cast_2d: ShapeCast2D = $ShapeCast2D
+@onready var fish: Sprite2D = $"../Fish"
+@onready var pond_clean: StaticBody2D = $"../PondClean"
+@onready var pond_dirty: StaticBody2D = $"../PondDirty"
 
 func _physics_process(delta: float) -> void:
 	if _can_move:
 		if not is_on_floor():
 			velocity += get_gravity() * delta
+			
+		if _fish_in_hand:
+			fish.position = position
 
 		if Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
@@ -33,12 +43,40 @@ func _physics_process(delta: float) -> void:
 			_is_hiding = true
 
 		move_and_slide()
-		for i in get_slide_collision_count():
-			if get_slide_collision(i).get_collider().name == "Owl":
+		
+		
+		for i in shape_cast_2d.get_collision_count():
+			
+			if shape_cast_2d.get_collider(i).name == "Owl":
+				
 				game_lost.emit()
 				_can_move = false
-			elif get_slide_collision(i).get_collider().name == "HidingSpot":
-				_hiding_spot_position = get_slide_collision(i).get_position()
+				
+			elif shape_cast_2d.get_collider(i).name == "HidingSpot":
+				
+				_hiding_spot_position = shape_cast_2d.target_position
+				# Fix later :)
+				
+			elif shape_cast_2d.get_collider(i).name == "PondDirty":
+				
+				if Input.is_action_just_pressed("action") and _fish_in_dirty_pond:
+					_fish_in_dirty_pond = false
+					_fish_in_hand = true
+				elif Input.is_action_just_pressed("action") and not _fish_in_dirty_pond and _fish_in_hand:
+					_fish_in_dirty_pond = true
+					_fish_in_hand = false
+					fish.position = pond_dirty.position
+					
+			elif shape_cast_2d.get_collider(i).name == "PondClean":
+				
+				if Input.is_action_just_pressed("action") and not _fish_in_dirty_pond and _fish_in_hand:
+					_fish_in_dirty_pond = false
+					_fish_in_hand = false
+					fish.position = pond_clean.position
+				elif Input.is_action_just_pressed("action") and not _fish_in_dirty_pond and not _fish_in_hand:
+					_fish_in_dirty_pond = false
+					_fish_in_hand = true
+					
 			else:
 				_hiding_spot_position = Vector2.ZERO
 	

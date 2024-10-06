7bef46fc4581e70185c193fb8e6c8aebfc4bf1f2
Camden Hovell <camdenhovell@gmail.com>
2024-10-06 16:08:34 -0400

    Add tutorial and background

--- project/title/title.gd
+++ project/title/title.gd
@@ -1,6 +1,25 @@
 extends Node2D
 
 
-func _on_button_pressed() -> void:
+@onready var tutorial: VBoxContainer = $Tutorial
+@onready var color_rect: ColorRect = $ColorRect
+@onready var exit_tutorial_button: Button = $ExitTutorialButton
+
+
+func _on_play_button_pressed() -> void:
 	AudioManager.play_menu_select()
 	get_tree().change_scene_to_file("res://level/level.tscn")
+
+
+func _on_tutorial_button_pressed() -> void:
+	AudioManager.play_menu_select()
+	tutorial.show()
+	color_rect.show()
+	exit_tutorial_button.show()
+
+
+func _on_exit_tutorial_button_pressed() -> void:
+	AudioManager.play_menu_select()
+	tutorial.hide()
+	color_rect.hide()
+	exit_tutorial_button.hide()
